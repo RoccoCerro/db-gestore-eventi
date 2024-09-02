de010b4197e4e96eb3eecbb7cdd9c6a908b08fc5
RoccoCerro <rcerro02@gmail.com>
2024-09-02 14:58:51 +0200

    Refactor: project template

--- routes/web.php
+++ routes/web.php
@@ -29,13 +29,13 @@ Route::middleware(['auth','verified'])
         return view('admin.dashboard');
     })->name('dashboard');
 
-    Route::resource('projects', ProjectController::class);
+    // Route::resource('projects', ProjectController::class);
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
