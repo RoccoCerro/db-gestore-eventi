4545093879328a25093bf1fb71982825cf4cf448
RoccoCerro <rcerro02@gmail.com>
2024-09-02 16:27:54 +0200

    Add: model event

---
+++ app/Models/Event.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'start',
+        'duration',
+        'image',
+        'price',
+        'total_tickets',
+        'end_of_sale',
+        'location_id',
+        'user_id'
+    ];
+}
